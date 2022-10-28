71a5c5c96c37c43af53c06e1c6fda337160d19b1
Sabit Murzaliev <sabit.murzaliev@gmail.com>
2022-10-28 23:56:48 +0600

    project init

---
+++ src/main/java/kz/sab1tm/belka/BelkaApplication.java
@@ -0,0 +1,13 @@
+package kz.sab1tm.belka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BelkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/sab1tm/belka/BelkaApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.sab1tm.belka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
